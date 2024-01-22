1f8b9996543f3b7a5db8bc5e33e2fbc6b1ee781b
Nick Rokosz <nicholasrokosz@gmail.com>
2024-01-21 22:13:12 -0600

    Add project.nvim

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -26,6 +26,7 @@ return {
         { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
         { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
         { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
+        { action = "Telescope projects",                                       desc = " Projects",        icon = " ", key = "p" },
         { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
         { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
         { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
