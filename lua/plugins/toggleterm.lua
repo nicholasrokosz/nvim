7aab32b79a028a084b81079454fdb2ba80020980
Nick Rokosz <nicholasrokosz@gmail.com>
2024-01-24 23:35:49 -0600

    Add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  keys = {
+    { "<space>t", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+  },
+  opts = {
+    direction = "float",
+  },
+}
